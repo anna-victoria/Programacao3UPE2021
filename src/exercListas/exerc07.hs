b76506730c6bccd155f5d539eb50f3d3b4fd550e
anna0victoria <anna.feliciana@upe.br>
2021-11-12 22:17:36 -0300

    Create exerc07.hs
    não consegui fazer

---
+++ src/exercListas/exerc07.hs
@@ -0,0 +1,5 @@
+-- Mostre que a compreensão de lista [(x,y) | x <- [1,2], y <- [3,4]], 
+-- com dois geradores, pode ser representada usando duas compreensões de lista, cada uma com apenas um gerador.
+
+concat :: [[a]] -> [a]
+
