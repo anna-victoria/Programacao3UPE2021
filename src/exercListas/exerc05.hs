30094341b18afff593a9442732b9c4e68c96f1af
anna0victoria <anna.feliciana@upe.br>
2021-11-12 22:18:16 -0300

    Create exerc05.hs
    não consegui fazer

---
+++ src/exercListas/exerc05.hs
@@ -0,0 +1,7 @@
+-- Uma tupla (x,y,z)de inteiros positivos é Pitagoreana se satisfaz a equação x² + y² = z². 
+-- Usando compreensão de listas com três geradores, defina a função
+-- pitag :: Int -> [(Int, Int, Int)] que retorna uma lista de todas as tuplas que satisfazem a condição estabelecida 
+-- e cujos componentes são menores ou iguais a um dado limite.
+
+pitag :: Int -> [(Int, Int, Int)]
+pitag a = []
